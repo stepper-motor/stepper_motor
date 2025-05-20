277974d90e82e381732c01c5670fb1fdfe227e24
Julik Tarkhanov <me@julik.nl>
2025-05-20 11:44:16 +0200

    Improve comment on ForwardScheduler

--- lib/stepper_motor/forward_scheduler.rb
+++ lib/stepper_motor/forward_scheduler.rb
@@ -6,13 +6,17 @@
 # option if your ActiveJob adapter supports far-ahead scheduling. Some adapters,
 # such as SQS, have limitations regarding the maximum delay after which a message
 # will become visible. For SQS, the limit is 900 seconds. If the job is further in the future,
-# it is likely going to fail to get enqueued. If you are working with a queue adapter
-# either:
+# it is likely going to fail to get enqueued. If you are working with a queue adapter that:
 #
 # * Does not allow easy introspection of jobs in the future (like Redis-based queues)
 # * Limits the value of the `wait:` parameter
 #
-# this scheduler is not a good fit for you, and you will need to use the {CyclicScheduler} instead.
+# this scheduler may not be a good fit for you, and you will need to use the {CyclicScheduler} instead.
+# Note that this scheduler is also likely to populate your queue with a high number of "far out"
+# jobs to be performed in the future. Different ActiveJob adapters are known to have varying
+# performance depending on the number of jobs in the queue. For example, good_job is known to
+# struggle a bit if the queue contains a large number of jobs (even if those jobs are not yet
+# scheduled to be performed). For good_job the {CyclicScheduler} is also likely to be a better option.
 class StepperMotor::ForwardScheduler
   def schedule(journey)
     StepperMotor::PerformStepJobV2
