3b7e9f51702d979823dc3981dbcf76efab3a7def
Julik Tarkhanov <me@julik.nl>
2025-05-23 01:40:41 +0200

    Implement idempotency keys
    Only the "perform step job" that has been scheduled for this particular execution is going to be able to execute the Journey. This is to further couple the "perform step" and the job enqueued to trigger it.

--- lib/stepper_motor/perform_step_job_v2.rb
+++ lib/stepper_motor/perform_step_job_v2.rb
@@ -3,9 +3,9 @@
 require "active_job"
 
 class StepperMotor::PerformStepJobV2 < ActiveJob::Base
-  def perform(journey_id:, journey_class_name:, **)
+  def perform(journey_id:, journey_class_name:, idempotency_key: nil, **)
     journey = StepperMotor::Journey.find(journey_id)
-    journey.perform_next_step!
+    journey.perform_next_step!(idempotency_key: idempotency_key)
   rescue ActiveRecord::RecordNotFound
     # The journey has been canceled and destroyed previously or elsewhere
   end
---
+++ test/dummy/db/migrate/20250522212312_stepper_motor_migration_003.rb
@@ -0,0 +1,5 @@
+class StepperMotorMigration003 < ActiveRecord::Migration[7.2]
+  def change
+    add_column :stepper_motor_journeys, :idempotency_key, :string, null: true
+  end
+end
