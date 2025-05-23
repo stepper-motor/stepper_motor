e39d42e080de994fdc7d569edb7ce0cf57e31d36
Julik Tarkhanov <me@julik.nl>
2025-05-23 16:46:53 +0200

    Expand step definition tests

--- test/stepper_motor/journey/step_definition_test.rb
+++ test/stepper_motor/journey/step_definition_test.rb
@@ -98,6 +98,47 @@ class StepDefinitionTest < ActiveSupport::TestCase
     assert_equal "step_2", step_two.name
   end
 
+  test "adds steps to step_definitions" do
+    journey_class = create_journey_subclass do
+      step :one do
+        # noop
+      end
+      step :two, wait: 20.minutes do
+        # noop
+      end
+    end
+
+    assert_kind_of Array, journey_class.step_definitions
+    assert_equal 2, journey_class.step_definitions.length
+
+    step_one, step_two = *journey_class.step_definitions
+
+    assert_equal "one", step_one.name
+    assert_equal 0, step_one.wait
+
+    assert_equal "two", step_two.name
+    assert_equal 20.minutes, step_two.wait
+  end
+
+  test "gives automatic names to anonymous steps" do
+    journey_class = create_journey_subclass do
+      step :one do
+        # noop
+      end
+      step wait: 20.minutes do
+        # noop
+      end
+    end
+
+    assert_kind_of Array, journey_class.step_definitions
+    assert_equal 2, journey_class.step_definitions.length
+
+    step_one, step_two = *journey_class.step_definitions
+
+    assert_equal "one", step_one.name
+    assert_equal "step_2", step_two.name
+  end
+
   test "does not allow invalid values for after: and wait:" do
     assert_raises(ArgumentError) do
       create_journey_subclass do
