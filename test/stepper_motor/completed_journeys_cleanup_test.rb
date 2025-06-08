24a14ddaffed5f0998f1319317106d6440c59154
Julik Tarkhanov <me@julik.nl>
2025-06-08 11:30:19 +0100

    Add test for when cleanup is disabled

--- test/stepper_motor/completed_journeys_cleanup_test.rb
+++ test/stepper_motor/completed_journeys_cleanup_test.rb
@@ -72,4 +72,25 @@ class CompletedJourneysCleanupTest < ActiveSupport::TestCase
   ensure
     StepperMotor.delete_completed_journeys_after = previous_setting
   end
+
+  test "does not delete any journeys if the setting is set to nil" do
+    previous_setting = StepperMotor.delete_completed_journeys_after
+    StepperMotor.delete_completed_journeys_after = nil
+
+    journey_class = create_journey_subclass do
+      step wait: 20.minutes do
+        # noop
+      end
+    end
+    journey = journey_class.create!
+    journey.cancel!
+
+    travel_to Time.current + 365.days
+    assert_no_changes "StepperMotor::Journey.count" do
+      StepperMotor::DeleteCompletedJourneysJob.new.perform
+    end
+    assert_nothing_raised { journey.reload }
+  ensure
+    StepperMotor.delete_completed_journeys_after = previous_setting
+  end
 end
