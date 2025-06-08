5eda843395243e2e99bb58e5086179c8fc7e786a
Julik Tarkhanov <me@julik.nl>
2025-06-08 23:26:14 +0200

    Regenerate schema

---
+++ test/dummy/db/migrate/20250608212541_stepper_motor_migration_005.rb
@@ -0,0 +1,17 @@
+class StepperMotorMigration005 < ActiveRecord::Migration[7.2]
+  def change
+    # Deduce whether the foreign key should be a UUID or not
+    journey_id_column = StepperMotor::Journey.columns_hash.fetch("id")
+    same_id_type_as_journey = journey_id_column.type
+
+    create_table :stepper_motor_task_handles, id: same_id_type_as_journey do |t|
+      t.references :journey, foreign_key: {to_table: "stepper_motor_journeys", on_delete: :cascade}
+      t.datetime :scheduled_at
+      t.string :idempotency_key, null: false
+      t.string :active_job_id, null: true
+      t.timestamps
+    end
+
+    add_index :stepper_motor_task_handles, [:journey_id, :idempotency_key], unique: true
+  end
+end
---
+++ test/dummy/db/schema.rb
@@ -0,0 +1,49 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.2].define(version: 2025_06_08_212541) do
+  create_table "stepper_motor_journeys", force: :cascade do |t|
+    t.string "type", null: false
+    t.string "state", default: "ready"
+    t.string "hero_type"
+    t.bigint "hero_id"
+    t.boolean "allow_multiple", default: false
+    t.string "previous_step_name"
+    t.string "next_step_name"
+    t.datetime "next_step_to_be_performed_at"
+    t.bigint "steps_entered", default: 0, null: false
+    t.bigint "steps_completed", default: 0, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "idempotency_key"
+    t.index ["hero_type", "hero_id"], name: "index_stepper_motor_journeys_on_hero_type_and_hero_id"
+    t.index ["next_step_to_be_performed_at"], name: "index_stepper_motor_journeys_on_next_step_to_be_performed_at", where: "state = 'ready' /*application='Dummy'*/"
+    t.index ["type", "hero_id", "hero_type"], name: "idx_journeys_one_per_hero_with_paused", unique: true, where: "allow_multiple = '0' AND state IN ('ready', 'performing', 'paused') /*application='Dummy'*/"
+    t.index ["type", "hero_type", "hero_id"], name: "index_stepper_motor_journeys_on_type_and_hero_type_and_hero_id"
+    t.index ["type"], name: "index_stepper_motor_journeys_on_type"
+    t.index ["updated_at"], name: "index_stepper_motor_journeys_on_updated_at", where: "state = 'canceled' OR state = 'finished' /*application='Dummy'*/"
+    t.index ["updated_at"], name: "stuck_journeys_index", where: "state = 'performing' /*application='Dummy'*/"
+  end
+
+  create_table "stepper_motor_task_handles", force: :cascade do |t|
+    t.integer "journey_id"
+    t.datetime "scheduled_at"
+    t.string "idempotency_key", null: false
+    t.string "active_job_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["journey_id", "idempotency_key"], name: "idx_on_journey_id_idempotency_key_2a95f45162", unique: true
+    t.index ["journey_id"], name: "index_stepper_motor_task_handles_on_journey_id"
+  end
+
+  add_foreign_key "stepper_motor_task_handles", "stepper_motor_journeys", column: "journey_id", on_delete: :cascade
+end
