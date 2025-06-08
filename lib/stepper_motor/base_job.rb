f239f552b1ad440a5a9383279266b4a49808d3c9
Julik Tarkhanov <me@julik.nl>
2025-06-08 13:06:58 +0100

    Explain the base job in a comment

--- lib/stepper_motor/base_job.rb
+++ lib/stepper_motor/base_job.rb
@@ -1,2 +1,5 @@
+# All StepperMotor job classes inherit from this one. It is available for
+# extension from StepperMotor.extend_base_job_class so that you can set
+# priority, include and prepend modules and so forth.
 class StepperMotor::BaseJob < ActiveJob::Base
 end
